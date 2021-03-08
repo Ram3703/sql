6382b6c07f67784219cdc81b114f5d9b44ed6726
Ram3703 <ramprasad.mallela@ltts.com>
2021-03-08 17:20:33 +0530

    Mini_Project

---
+++ miniproject/src/com/ltts/configuration/MyConnection.java
@@ -0,0 +1,16 @@
+package com.ltts.configuration;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MyConnection {
+	
+	public static Connection getConnection() throws Exception
+	{
+		
+		Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/ram","root","root");
+		return c;
+		
+	}
+	
+
+}
